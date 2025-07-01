3f94c142a34d400ae7a64cad22db7616e30ed138
siusr <cartesius1001@gamil.com>
2025-07-01 19:11:51 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -12,17 +12,29 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimal,
+        uint256 _amount
+    ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
         // transction
         // from, to, data, value, gas, ...
-        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
+        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
 
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
     }
 }
